ebd3bde47cd9620538e69259f3bf5ed5b64299ff
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-11-02 11:13:28 +0200

    Update evaluareformativa.py

--- evaluareformativa.py
+++ evaluareformativa.py
@@ -10,29 +10,27 @@ print("a) Produsele expirate:")
 for i in range(0, len(p)):
     if p[i][3] <= datacurenta:
         print(p[i][1])
-    else:
-        print("Nu sunt produse expirate")
-print("")
+    
+print("b) Produsele cu reducere de 50%:")
+for i in range(0, len(p)):
+    if p[i][3]*0.25 == datacurenta:
+        print(p[i][1])
 
 print("c) Produsele cu reducere de 20%:")
 for i in range(0, len(p)):
     if datacurenta == (p[i][2]+p[i][3]/2):
         print(p[i][1])
-    else:
-        print("Nu sunt produse la reducere cu 20%")
-
-print("b) Produsele cu reducere de 50%:")
-for i in range(0, len(p)):
-    if p[i][3]*0.25 == datacurenta:
-        print(p[i][1])
-    else:
-        print("Nu sunt produse la reducere cu 50%")
-
+  
 
 print("d) Produsele cu termen de valabilitate de cel putin 1 an:")
-print("Nu sunt produse cu termen de valabilitate de cel putin 1 an")
-
+for i in range(0, len(p)):
+    if p[i][2]+p[i][3]>365:
+        print(p[i][1])
+    
 print("e) Lista produselor cu termen de valabilitate de cel mult o luna:")
+for i in range(0, len(p)):
+    if p[i][2]+p[i][2]>30:
+        print(p[i][1])
 print("Nu sunt produse cu termen de valabilitate de cel mult o luna")
 
 
